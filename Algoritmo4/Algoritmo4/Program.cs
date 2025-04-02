cb470ec9853d5c034a6348a7e14e769937b7713a
Ana Carolina Machado <anacarolinamachado366@gmail.com>
2025-04-01 21:36:05 -0300

    CONCLUSÃO DO EXERCICIO 4

---
+++ Algoritmo4/Algoritmo4/Program.cs
@@ -0,0 +1,18 @@
+﻿using System.Globalization;
+
+//Variaveis
+double largura;
+double comprimento, area, valorMetro, valorTerreno;
+
+//Entrada
+largura = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+comprimento = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+valorMetro = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+//Calculo
+area = largura * comprimento;
+valorTerreno = area * valorMetro;
+
+//Saida
+Console.WriteLine("AREA - " + area.ToString("F2",CultureInfo.InvariantCulture));
+Console.WriteLine("PREÇO = " + valorTerreno.ToString("F2" ,CultureInfo.InvariantCulture));
